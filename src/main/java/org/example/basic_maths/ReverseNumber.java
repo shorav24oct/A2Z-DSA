1956570e3164f45b8d38f0f1885f3c4734ee4ab3
shorav24oct <sharmasaurav142@gmail.com>
2025-12-17 07:46:43 +0530

    handle overflow as well.

--- src/main/java/org/example/basic_maths/ReverseNumber.java
+++ src/main/java/org/example/basic_maths/ReverseNumber.java
@@ -2,14 +2,25 @@ package org.example.basic_maths;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        System.out.println(reverse(123456));
+        System.out.println(reverse(1534236469));
     }
 
     private static int reverse(int no) {
         int rev = 0;
-        while (no > 0) {
-            int ld = no % 10;
-            rev = (rev * 10) + ld;
+
+        while (no != 0) {
+            int lastDigit = no % 10;
+
+            // 🔴 Overflow check BEFORE multiplying by 10
+            if (rev > Integer.MAX_VALUE / 10 || rev == Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+
+            if (rev < Integer.MIN_VALUE / 10 || rev == Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = (rev * 10) + lastDigit;
             no = no / 10;
         }
         return rev;
