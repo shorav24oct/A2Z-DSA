04a658044c66511648d345d02f5fa4b08b79160c
shorav24oct <sharmasaurav142@gmail.com>
2025-12-18 08:12:49 +0530

    add method printDev to better time complexity.

--- src/main/java/org/example/basic_maths/PrintAllDivisors.java
+++ src/main/java/org/example/basic_maths/PrintAllDivisors.java
@@ -1,8 +1,11 @@
 package org.example.basic_maths;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintAllDivisors {
     public static void main(String[] args) {
-        printDivisors(36);
+        printDiv(36);
     }
 
     /*
@@ -13,5 +16,21 @@ public class PrintAllDivisors {
             if (no % i == 0)
                 System.out.print(i + " ");
         }
+    }
+
+    /*
+    Time Complexity: Big-O(sqrt(n))
+     */
+    private static void printDiv(int no) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i * i <= no; i++) {
+            if (no % i == 0) {
+                list.add(i);
+                if ((no / i) != i) {
+                    list.add(no / i);
+                }
+            }
+        }
+        list.stream().sorted().forEach(System.out::println);
     }
 }
