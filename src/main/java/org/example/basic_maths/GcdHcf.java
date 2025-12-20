6f6fd3b86447adbefecc57cfacb18fb7ff5aa02c
shorav24oct <sharmasaurav142@gmail.com>
2025-12-20 07:40:47 +0530

    using euclidean algo.

--- src/main/java/org/example/basic_maths/GcdHcf.java
+++ src/main/java/org/example/basic_maths/GcdHcf.java
@@ -2,7 +2,7 @@ package org.example.basic_maths;
 
 public class GcdHcf {
     public static void main(String[] args) {
-        System.out.println(printGcdOrHcf(11, 13));
+        System.out.println(printGCD(11, 13));
     }
 
     /*
@@ -11,11 +11,28 @@ public class GcdHcf {
     private static int printGcdOrHcf(int n1, int n2) {
         int gcd = 0;
         for (int i = Math.min(n1, n2); i >= 1; i--) {
-            if ( (n1 % i == 0) && (n2 % i == 0) ) {
+            if ((n1 % i == 0) && (n2 % i == 0)) {
                 gcd = i;
                 break;
             }
         }
         return gcd;
+    }
+
+    /*
+    Euclidean algorithm
+    Time Complexity: Big-O(log(min(n1,n2)))
+     */
+    static int printGCD(int n1, int n2) {
+        while (n1 > 0 && n2 > 0) {
+            if (n1 > n2)
+                n1 = n1 % n2;
+            else
+                n2 = n2 % n1;
+        }
+        if (n1 == 0)
+            return n2;
+        else
+            return n1;
     }
 }
