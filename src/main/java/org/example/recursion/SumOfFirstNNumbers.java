649208b393031b6c19ccb7108d102c816d5bc6dc
shorav24oct <sharmasaurav142@gmail.com>
2025-12-21 12:17:28 +0530

    sum of first N numbers.

--- src/main/java/org/example/recursion/SumOfFirstNNumbers.java
+++ src/main/java/org/example/recursion/SumOfFirstNNumbers.java
@@ -3,8 +3,9 @@ package org.example.recursion;
 public class SumOfFirstNNumbers {
     public static void main(String[] args) {
         int sum = 0;
-        int n = 4;
+        int n = 5;
         sumOfNNumbers(n, sum);
+        System.out.println(sumOfN(5));
     }
 
     private static void sumOfNNumbers(int n, int sum) {
@@ -13,5 +14,12 @@ public class SumOfFirstNNumbers {
             return;
         }
         sumOfNNumbers(n-1, sum + n);
+    }
+
+    private static int sumOfN(int N) {
+        if (N == 0)
+            return 0;
+
+        return N + sumOfN(N - 1);
     }
 }
