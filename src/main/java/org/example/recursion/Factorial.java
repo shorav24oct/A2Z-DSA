aa76eaad34c0509c8df1eabd740386d7eb4757b5
shorav24oct <sharmasaurav142@gmail.com>
2025-12-21 12:30:08 +0530

    factorial

---
+++ src/main/java/org/example/recursion/Factorial.java
@@ -0,0 +1,14 @@
+package org.example.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(4));
+    }
+
+    private static int fact(int i) {
+        if (i == 1)
+            return 1;
+
+        return i * fact(i - 1);
+    }
+}
