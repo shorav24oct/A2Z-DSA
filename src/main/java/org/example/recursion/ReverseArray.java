704392aed5969d8eb2b654078b0865ab71d3d26e
shorav24oct <sharmasaurav142@gmail.com>
2025-12-22 07:40:48 +0530

    reverse an array using single variable

--- src/main/java/org/example/recursion/ReverseArray.java
+++ src/main/java/org/example/recursion/ReverseArray.java
@@ -5,9 +5,13 @@ import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5};
+
         System.out.println("Array before swap");
         System.out.println(Arrays.toString(a));
-        reverseAnArray(a, 0, a.length - 1);
+
+        //reverseAnArray(a, 0, a.length - 1);
+        revArr(a, 0);
+
         System.out.println("Array after swap");
         System.out.println(Arrays.toString(a));
     }
@@ -21,6 +25,17 @@ public class ReverseArray {
 
         swap(a, left, right);
         reverseAnArray(a, left + 1, right - 1);
+    }
+
+    /*
+    using single variable
+     */
+    private static void revArr(int[] a, int i) {
+        if (i >= (a.length / 2))
+            return;
+
+        swap(a, i, a.length - i - 1);
+        revArr(a, i + 1);
     }
 
     private static void swap(int[] a, int left, int right) {
