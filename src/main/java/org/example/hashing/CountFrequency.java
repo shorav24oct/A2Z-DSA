36f81bce66a5ddc606fa5028f835117fd213cab6
shorav24oct <sharmasaurav142@gmail.com>
2025-12-25 08:10:25 +0530

    count freq using map

--- src/main/java/org/example/hashing/CountFrequency.java
+++ src/main/java/org/example/hashing/CountFrequency.java
@@ -1,16 +1,29 @@
 package org.example.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 count-frequency-of-each-element-in-the-array
  */
 public class CountFrequency {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1, 2, 3};
-        countFreqUsingArr(arr, arr.length);
+        //countFreqUsingArr(arr, arr.length);
         countFreqUsingMap(arr, arr.length);
     }
 
     private static void countFreqUsingMap(int[] arr, int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        // Traverse through the HashMap and print frequencies
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
 
     }
 
