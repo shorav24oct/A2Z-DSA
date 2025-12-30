7c915d0fbf39ef3f4414e35fa09b61855cc1abb2
shorav24oct <sharmasaurav142@gmail.com>
2025-12-30 07:26:55 +0530

    add time complexity

--- src/main/java/org/example/array/easy/FirstLargest.java
+++ src/main/java/org/example/array/easy/FirstLargest.java
@@ -5,22 +5,29 @@ import java.util.Arrays;
 public class FirstLargest {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        bruteForce(arr);
-        optimal(arr);
+        int n = arr.length;
+        bruteForce(arr, n);
+        optimal(arr, n);
     }
 
-    private static void optimal(int[] arr) {
+    /*
+    TC: Big-O(n)
+     */
+    private static void optimal(int[] arr, int n) {
         int large = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < n; i++) {
             if (arr[i] > large)
                 large = arr[i];
         }
         System.out.println(large);
     }
 
-    private static void bruteForce(int[] arr) {
+    /*
+    TC: Big-O(n log n)
+     */
+    private static void bruteForce(int[] arr, int n) {
         Arrays.sort(arr);
-        System.out.println(arr[arr.length - 1]);
+        System.out.println(arr[n - 1]);
     }
 }
