e86d5710411e030c58a10c34623ff86af239476e
shorav24oct <sharmasaurav142@gmail.com>
2026-01-02 19:30:34 +0530

    right rotate array by 1 place

--- src/main/java/org/example/array/easy/RightRotateBy1.java
+++ src/main/java/org/example/array/easy/RightRotateBy1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RightRotateBy1 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4};
         int n = arr.length;
         rotateBy1(arr, n);
     }
@@ -14,12 +14,12 @@ public class RightRotateBy1 {
     SC: Big-O(1)
      */
     private static void rotateBy1(int[] arr, int n) {
-        int temp = arr[0];
+        int temp = arr[n - 1];
 
-        for (int i = 1; i < n; i++) {
-            arr[i - 1] = arr[i];
+        for (int i = n - 2; i >= 0; i--) {
+            arr[i + 1] = arr[i];
         }
-        arr[n - 1] = temp;
+        arr[0] = temp;
         System.out.println(Arrays.toString(arr));
     }
 }
