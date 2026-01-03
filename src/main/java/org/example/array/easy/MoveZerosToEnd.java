9d6902c4782748621ef3788f733b5413986e1369
shorav24oct <sharmasaurav142@gmail.com>
2026-01-03 13:44:08 +0530

    move zeros to end using optimal

--- src/main/java/org/example/array/easy/MoveZerosToEnd.java
+++ src/main/java/org/example/array/easy/MoveZerosToEnd.java
@@ -4,10 +4,43 @@ import java.util.Arrays;
 
 public class MoveZerosToEnd {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 0, 0, 4, 0, 5};
+        int[] arr = {1, 0, 2, 3, 0};
         int n = arr.length;
         System.out.println(Arrays.toString(arr));
-        moveZeros(arr, n);
+        //moveZeros(arr, n);
+        moveZerosOptimal(arr, n);
+    }
+
+    /*
+    Use 2 pointer approach
+    TC: Big-O(N)
+    SC: Big-O(1)
+     */
+    private static void moveZerosOptimal(int[] arr, int n) {
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < n; i++) {
+            // If current element is non-zero
+            if (arr[i] != 0) {
+                // Swap with arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                // Move j to next zero
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
     /*
