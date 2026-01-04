28fe09636517d98d437a6f621f44adf81fb5a614
shorav24oct <sharmasaurav142@gmail.com>
2026-01-04 08:08:38 +0530

    union of 2 sorted arrays.

--- src/main/java/org/example/array/easy/Union.java
+++ src/main/java/org/example/array/easy/Union.java
@@ -1,13 +1,11 @@
 package org.example.array.easy;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Union {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 2, 3, 4, 5};
-        int[] arr2 = {2, 2, 3, 4, 6};
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {2, 3, 4};
 
         unionOfTwoArrays(arr1, arr2, arr1.length, arr2.length);
     }
@@ -20,29 +18,47 @@ public class Union {
     private static void unionOfTwoArrays(int[] arr1, int[] arr2, int n1, int n2) {
         int i = 0;
         int j = 0;
-        Set<Integer> unionSet = new LinkedHashSet<>();
+
+        // List to store union elements
+        List<Integer> Union = new ArrayList<>();
 
         while (i < n1 && j < n2) {
+            // If element in arr1 is smaller
             if (arr1[i] < arr2[j]) {
-                unionSet.add(arr1[i]);
-                i++;
+                // Add if empty or not duplicate
+                // skipping duplicates by checking only the last inserted element.
+                if (Union.isEmpty() || Union.get(Union.size() - 1) != arr1[i]) {
+                    Union.add(arr1[i]);
+                    i++;
+                }
             }
 
+            // If element in arr2 is smaller
             else if (arr2[j] < arr1[i]) {
-                unionSet.add(arr2[j]);
-                j++;
-            }
-
-            while (i < n1) {
-                unionSet.add(arr1[i]);
-                i++;
-            }
-
-            while (j < n2) {
-                unionSet.add(arr2[j]);
-                j++;
+                // Add if empty or not duplicate
+                // skipping duplicates by checking only the last inserted element.
+                if (Union.isEmpty() || Union.get(Union.size() - 1) != arr2[j]) {
+                    Union.add(arr2[j]);
+                    j++;
+                }
+            } else {
+                // Elements are equal, add once if not duplicate
+                if (Union.isEmpty() || Union.get(Union.size() - 1) != arr1[i])
+                    Union.add(arr1[i]);
+                i++; j++;  // Move both pointers
             }
         }
-        System.out.println(Arrays.toString(unionSet.toArray()));
+
+        while (i < n1) {
+            Union.add(arr1[i]);
+            i++;
+        }
+
+        while (j < n2) {
+            Union.add(arr2[j]);
+            j++;
+        }
+
+        System.out.println(Arrays.toString(Union.toArray()));
     }
 }
