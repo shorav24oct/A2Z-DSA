6e2071dcea48fa87442996b0c6c1296e9ae213e4
shorav24oct <sharmasaurav142@gmail.com>
2026-01-05 08:01:50 +0530

    intersection of 2 sorted arrays optimal.

--- src/main/java/org/example/array/easy/Intersection.java
+++ src/main/java/org/example/array/easy/Intersection.java
@@ -6,12 +6,36 @@ import java.util.List;
 public class Intersection {
     public static void main(String[] args) {
         int[] a = {1, 1, 2, 3, 4};
-        int[] b = {1, 2, 3, 5};
+        int[] b = {1, 2, 5};
 
         int n1 = a.length;
         int n2 = b.length;
 
-        intersect(a, b, n1, n2);
+        //intersect(a, b, n1, n2);
+        intersectOptimal(a, b, n1, n2);
+    }
+
+    /*
+    Optimal Approach
+    TC: Big-O(n1 + n2)
+    SC: Big-O(1)
+     */
+    private static void intersectOptimal(int[] a, int[] b, int n1, int n2) {
+        int  i = 0, j = 0;
+
+        List<Integer> ans = new ArrayList<>();
+
+        while (i < n1 && j < n2) {
+            if (a[i] < b[j])
+                i++;
+            else if (b[j] > a[i])
+                j++;
+            else {
+                ans.add(a[i]);
+                i++; j++;
+            }
+        }
+        System.out.println(ans);
     }
 
     /*
