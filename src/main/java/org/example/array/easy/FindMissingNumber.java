6fc32af3a61f5abf2d138f1ffc0fbf5b3d60afe2
shorav24oct <sharmasaurav142@gmail.com>
2026-01-06 07:04:31 +0530

    find missing no using better approach

--- src/main/java/org/example/array/easy/FindMissingNumber.java
+++ src/main/java/org/example/array/easy/FindMissingNumber.java
@@ -2,9 +2,30 @@ package org.example.array.easy;
 
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5};
+        int[] arr = {1, 2, 4, 5};
         int N = 5;
-        System.out.println(missingNo(arr, N));
+
+        //System.out.println(missingNo(arr, N));
+        System.out.println(missing(arr, N));
+    }
+
+    /*
+    Better Approach use Hashing
+    TC: Big-O(2N)
+    SC: Big-O(N)
+     */
+    private static int missing(int[] arr, int n) {
+        int[] hash = new int[n + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            hash[arr[i]] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (hash[i] == 0)
+                return i;
+        }
+        return -1;
     }
 
     /*
