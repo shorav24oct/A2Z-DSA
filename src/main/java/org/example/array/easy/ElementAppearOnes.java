ba2f80df9bfbfafc6ce45326a3c8cf4b3ec41a50
shorav24oct <sharmasaurav142@gmail.com>
2026-01-08 14:58:13 +0530

    element appear one's using optimal

--- src/main/java/org/example/array/easy/ElementAppearOnes.java
+++ src/main/java/org/example/array/easy/ElementAppearOnes.java
@@ -3,9 +3,26 @@ package org.example.array.easy;
 public class ElementAppearOnes {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 3, 3, 4, 4};
-        System.out.println(bruteForce(arr, arr.length));
+        //System.out.println(bruteForce(arr, arr.length));
+        System.out.println(optimal(arr, arr.length));
     }
 
+    /*
+    TC: Big-O(n)
+    SC: Big-O(1)
+     */
+    private static int optimal(int[] arr, int n) {
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ arr[i];
+        }
+        return xor;
+    }
+
+    /*
+    TC: Big-O(n * n)
+    SC: Big-O(1)
+     */
     private static int bruteForce(int[] arr, int n) {
 
         for (int i = 0; i < n; i++) {
