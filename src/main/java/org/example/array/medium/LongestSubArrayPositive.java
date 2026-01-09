2b9352e3c83253f6996f09df17055f2a3f9da30e
shorav24oct <sharmasaurav142@gmail.com>
2026-01-09 08:09:59 +0530

    longest sub array with given sum K positive numbers only

---
+++ src/main/java/org/example/array/medium/LongestSubArrayPositive.java
@@ -0,0 +1,37 @@
+package org.example.array.medium;
+
+public class LongestSubArrayPositive {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
+        int k = 3;
+        System.out.println(brute(arr, arr.length, k));
+    }
+
+    /*
+    TC: Big-O(n*n*n)
+    SC: Big-O(1)
+     */
+    private static int brute(int[] arr, int n, int k) {
+        int maxLength = 0;
+
+        // starting Index
+        for (int startIndex = 0; startIndex < n; startIndex++) {
+            // ending Index
+            for (int endIndex = startIndex; endIndex < n; endIndex++) {
+
+                 /* add all the elements of
+                   sub-array = arr[startIndex...endIndex] */
+                int sum = 0;
+
+                for (int i = startIndex; i <= endIndex; i++) {
+                    sum += arr[i];
+                }
+
+                if (sum == k) {
+                    maxLength = Math.max(maxLength, endIndex - startIndex + 1);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
