fbaee8bedd87d9132a1f899eeb8bee35cac4cbbb
shorav24oct <sharmasaurav142@gmail.com>
2026-01-10 13:02:51 +0530

    longest sub array with given sum K positive using optimal approach.

--- src/main/java/org/example/array/medium/LongestSubArrayPositive.java
+++ src/main/java/org/example/array/medium/LongestSubArrayPositive.java
@@ -2,9 +2,42 @@ package org.example.array.medium;
 
 public class LongestSubArrayPositive {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-        int k = 3;
-        System.out.println(brute(arr, arr.length, k));
+        int[] arr = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+        //System.out.println(brute(arr, arr.length, k));
+        System.out.println(optimal(arr, arr.length, k));
+    }
+
+    /*
+    Optimal Approach using 2-pointer and sliding window technique
+    TC: Big-O(n)
+    SC: Big-O(1)
+     */
+    private static int optimal(int[] arr, int n, int k) {
+        int maxLength = 0;
+        int left = 0;
+        int right = 0;
+        int sum = arr[0];
+
+        while (right < n) {
+            // expand sub-array window if sum < k
+            right++;
+            if ( right < n) {
+                sum += arr[right];
+            }
+
+            // trim or shrink the window if sum > k
+            while (left <= right && sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            // Update the max length if sum = k
+            if (sum == k) {
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+        }
+        return maxLength;
     }
 
     /*
