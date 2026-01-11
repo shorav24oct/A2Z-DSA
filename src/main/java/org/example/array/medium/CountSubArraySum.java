642de79d7fae99f21d85b948846d994d8913cf7b
shorav24oct <sharmasaurav142@gmail.com>
2026-01-11 06:00:17 +0530

    total number of sub-arrays whose sum equals k using brute force.

--- src/main/java/org/example/array/medium/CountSubArraySum.java
+++ src/main/java/org/example/array/medium/CountSubArraySum.java
@@ -9,24 +9,55 @@ Explanation: The sub-arrays that sum up to 6 are [3, 1, 2] and [2, 4].
  */
 public class CountSubArraySum {
     public static void main(String[] args) {
-        int arr[] = {3, 1, 2, 4};
+        int arr[] = {3, 1, 2, 4, 2};
         int k = 6;
         System.out.println(bruteForce(arr, arr.length, k));
+        //System.out.println(optimal(arr, arr.length, k));
+    }
+
+    /*
+    Optimal Using 2 pointer and sliding window technique
+    TC: Big-O(n)
+    SC: Big-O(1)
+     */
+    private static int optimal(int[] arr, int n, int k) {
+        int left = 0, right = 0;
+        int maxL = 0;
+        int sum = arr[0];
+        int count = 0;
+
+        while (right < n) {
+            // expand window if sum < k
+            right++;
+            if (right < n) {
+                sum += arr[right];
+            }
+
+            // shrink window if sum > k
+            while (left <= right && sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            // update max length if sum = k
+            if (sum == k) {
+                maxL = Math.max(maxL, right - left + 1);
+                count++;
+            }
+        }
+        return count;
     }
 
     private static int bruteForce(int[] arr, int n, int k) {
-        int maxLength = 0;
         int count = 0;
 
-        for (int si = 0; si < n; si++) {
-            for (int ei = si; ei < n; ei++) {
-                int sum = 0;
-                for (int i = si; i <= ei; i++) {
-                    sum += arr[i];
-                }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
                 if (sum == k) {
                     count++;
-                    maxLength = Math.max(maxLength, ei - si + 1);
                 }
             }
         }
